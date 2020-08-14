7b9e7ec710e5d6a64977f8c09515375af42a704e
anhnguyen <nguyenanh@hotmail.de>
2020-08-14 17:44:41 +0200

    refactor custom col component

--- app/matestack/components/col.rb
+++ app/matestack/components/col.rb
@@ -1,29 +1,35 @@
 class Components::Col < Matestack::Ui::StaticComponent
-  def prepare
-    @col_classes = []
+
+  def response 
+    div id: "#{options[:id]}", class: "#{col_classes}" do
+      yield_components
+    end
+  end
+
+  protected
+
+  def col_classes
+    classes = []
 
     [:sm, :md, :lg, :xl, :xxl].each do |size| 
-      @col_classes << "col-#{size}-#{options[size]}" if options[size].present?
+      classes << "col-#{size}-#{options[size]}" if options[size].present?
     end
-    @col_classes << "col-#{options[:default]}" if options[:default].present?
+    classes << "col-#{options[:default]}" if options[:default].present?
 
     [:order, :order_sm, :order_md, :order_lg, :order_xl, :order_xxl].each do |order| 
-      @col_classes << "#{order}-#{options[order]}".gsub('_', '-') if options[order].present?
+      classes << "#{order}-#{options[order]}".gsub('_', '-') if options[order].present?
     end
 
     [:offset, :offset_sm, :offset_md, :offset_lg, :offset_xl, :offset_xxl].each do |offset| 
-      @col_classes << "#{offset}-#{options[offset]}".gsub('_', '-') if options[offset].present?
+      classes << "#{offset}-#{options[offset]}".gsub('_', '-') if options[offset].present?
     end
 
-    @col_classes << "align-self-#{options[:align_self]}" if options[:align_self].present?
+    classes << "align-self-#{options[:align_self]}" if options[:align_self].present?
     # for others option
-    @col_classes << "#{options[:class]}" if options[:class].present?
-    @col_classes << "col" if @col_classes.blank?
-  end
+    classes << "col" if classes.blank?
 
-  def response 
-    div id: "#{options[:id]}", class: "#{@col_classes.join(' ')}" do
-      yield_components
-    end
+    #optional classes
+    classes << @options[:class]
+    classes.join(' ') 
   end
 end
