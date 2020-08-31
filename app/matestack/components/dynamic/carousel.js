145db6af90e39f839edf79958e49453f367862b6
anhnguyen <nguyenanh@hotmail.de>
2020-08-31 14:18:50 +0200

    carousel instance

--- app/matestack/components/dynamic/carousel.js
+++ app/matestack/components/dynamic/carousel.js
@@ -2,10 +2,11 @@ MatestackUiCore.Vue.component('matestack-ui-bootstrap-carousel', {
   mixins: [MatestackUiCore.componentMixin],
   data() {
     return {
-      alertInstance: undefined
+      carouselInstance: undefined
     };
   },
   mounted() {
     var myCarousel = document.getElementById(this.componentConfig["carousel-id"])
+    this.carouselInstance = new bootstrap.Carousel(myCarousel)
   }
 });
