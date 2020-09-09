573b2ed0c766d82eb0254168d22a933ef47f9a3a
anhnguyen <nguyenanh@hotmail.de>
2020-09-09 13:49:58 +0200

    add sr_only option

--- app/matestack/components/badge.rb
+++ app/matestack/components/badge.rb
@@ -1,12 +1,13 @@
 class Components::Badge < Matestack::Ui::StaticComponent
   
   optional class: { as: :bs_class }
-  optional :text, :color, :rounded
+  optional :text, :color, :rounded, :sr_only
   def response
     span badge_attributes do
       plain text if text
       yield_components
     end
+    span class: "sr-only", text: sr_only if sr_only.present?
   end
 
   protected
