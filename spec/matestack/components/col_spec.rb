680b22bddaba27584a65ee5e838c7252f6ce0fb3
anhnguyen <nguyenanh@hotmail.de>
2020-09-09 18:58:15 +0200

    [WIP][TEST] col component

--- spec/matestack/components/col_spec.rb
+++ spec/matestack/components/col_spec.rb
@@ -5,9 +5,32 @@ describe 'Components::Col', type: :feature, js: true do
 
   it 'renders default column' do
     matestack_render do
-      close dismiss: "modal"
+      row do
+        col do         
+          btn text: "Button A"
+        end
+        col do    
+          btn text: "Button B"      
+        end
+      end
     end
     visit example_path
-    expect(page).to have_selector('button.close[type=button]')
+    expect(page).to have_selector('div.col')
+  end
+
+  it 'renders column with breakpoints & alignment' do
+    matestack_render do
+      row do
+        col default: 12, lg: 4, align_self: :center do
+          btn text: "Button A"
+        end
+        col default: 12, md: 6, align_self: :start do
+          btn text: "Button B"      
+        end
+      end
+    end
+    visit example_path
+    expect(page).to have_selector('div.col-12.col-lg-4.align-self-center')
+    expect(page).to have_selector('div.col-12.col-md-6.align-self-start')
   end
 end
