c047895cdb8e2306d328b47469f98e7360a2ee43
anhnguyen <nguyenanh@hotmail.de>
2020-09-14 13:30:30 +0200

    adding row spec test

---
+++ spec/matestack/bootstrap/components/row_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'Bootstrap::Layout::Row', type: :feature, js: true do
+  include Utils
+
+  it 'should create a simple row' do
+    matestack_render { row }
+    visit example_path
+    expect(page).to have_selector('div.row')
+  end
+  it 'should create a row with vertical alignment' do
+    matestack_render { 
+      row horizontal: :center, class: "bg-secondary px-3" do
+        plain "Row"
+      end
+    }
+    visit example_path
+    expect(page).to have_selector('div.row.justify-content-center')
+  end
+  it 'should create a row with horizontal alignment' do
+    matestack_render { 
+      row vertical: :start do
+        plain "Row"
+      end
+    }
+    visit example_path
+    expect(page).to have_selector('div.row.align-items-start')
+  end
+  it 'should create a row with custom classes' do
+    matestack_render { 
+      row class: "bg-secondary" do
+        plain "Row"
+      end
+    }
+    visit example_path
+    expect(page).to have_selector('div.row.bg-secondary')
+  end
+end
