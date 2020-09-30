b32dd2747ca7bd17e2238173004c890c6857b9ea
anhnguyen <nguyenanh@hotmail.de>
2020-09-30 15:04:36 +0200

    adding standard interval option

--- app/matestack/bootstrap/components/carousel.js
+++ app/matestack/bootstrap/components/carousel.js
@@ -28,49 +28,49 @@ MatestackUiCore.Vue.component('matestack-ui-bootstrap-carousel', {
   mounted: function() {
     const self = this;
     var myCarousel = self.$el;
-    this.carouselInstance = new bootstrap.Carousel(myCarousel, {
-      interval: self.componentConfig['interval']
+    self.carouselInstance = new bootstrap.Carousel(myCarousel, {
+      interval: (self.componentConfig['interval'] || 5000 )
     })
   },
 
   created: function() {
     const self = this
-    if(this.componentConfig["cycle_on"] != undefined){
-      var cycle_events = this.componentConfig["cycle_on"].split(",")
+    if(self.componentConfig["cycle_on"] != undefined){
+      var cycle_events = self.componentConfig["cycle_on"].split(",")
       cycle_events.forEach(cycle_event => MatestackUiCore.matestackEventHub.$on(cycle_event.trim(), self.cycle));
     }
-    if(this.componentConfig["pause_on"] != undefined){
-      var pause_events = this.componentConfig["pause_on"].split(",")
+    if(self.componentConfig["pause_on"] != undefined){
+      var pause_events = self.componentConfig["pause_on"].split(",")
       pause_events.forEach(pause_event => MatestackUiCore.matestackEventHub.$on(pause_event.trim(), self.pause));
     }
-    if(this.componentConfig["prev_on"] != undefined){
-      var prev_events = this.componentConfig["prev_on"].split(",")
+    if(self.componentConfig["prev_on"] != undefined){
+      var prev_events = self.componentConfig["prev_on"].split(",")
       prev_events.forEach(prev_event => MatestackUiCore.matestackEventHub.$on(prev_event.trim(), self.prev));
     }
-    if(this.componentConfig["next_on"] != undefined){
-      var next_events = this.componentConfig["next_on"].split(",")
+    if(self.componentConfig["next_on"] != undefined){
+      var next_events = self.componentConfig["next_on"].split(",")
       next_events.forEach(next_event => MatestackUiCore.matestackEventHub.$on(next_event.trim(), self.next));
     }
   },
 
   beforeDestroy: function() {
     const self = this
-    if(this.componentConfig["cycle_on"] != undefined){
-      var cycle_events = this.componentConfig["cycle_on"].split(",")
+    if(self.componentConfig["cycle_on"] != undefined){
+      var cycle_events = self.componentConfig["cycle_on"].split(",")
       cycle_events.forEach(cycle_event => MatestackUiCore.matestackEventHub.$off(cycle_event.trim(), self.cycle));
     }
 
-    if(this.componentConfig["pause_on"] != undefined){
-      var pause_events = this.componentConfig["pause_on"].split(",")
+    if(self.componentConfig["pause_on"] != undefined){
+      var pause_events = self.componentConfig["pause_on"].split(",")
       pause_events.forEach(pause_event => MatestackUiCore.matestackEventHub.$off(pause_event.trim(), self.pause));
     }
 
-    if(this.componentConfig["prev_on"] != undefined){
-      var prev_events = this.componentConfig["prev_on"].split(",")
+    if(self.componentConfig["prev_on"] != undefined){
+      var prev_events = self.componentConfig["prev_on"].split(",")
       prev_events.forEach(prev_event => MatestackUiCore.matestackEventHub.$off(prev_event.trim(), self.prev));
     }
-    if(this.componentConfig["next_on"] != undefined){
-      var next_events = this.componentConfig["next_on"].split(",")
+    if(self.componentConfig["next_on"] != undefined){
+      var next_events = self.componentConfig["next_on"].split(",")
       next_events.forEach(next_event => MatestackUiCore.matestackEventHub.$off(next_event.trim(), self.next));
     }
   },
