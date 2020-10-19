407f775814bc559d6564a7cf2595892d323ce052
anhnguyen <nguyenanh@hotmail.de>
2020-10-19 16:41:19 +0200

    [WIP] generated basic bootstrap table

--- app/matestack/bootstrap/content/table.rb
+++ app/matestack/bootstrap/content/table.rb
@@ -1,8 +1,97 @@
 class Bootstrap::Content::Table < Matestack::Ui::Component
+  include Matestack::Ui::Core::Collection::Helper
+
+  optional class: { as: :bs_class }, id: { as: :bs_id }
+  # Smart Table Attributes
+  optional :include, :filter, :base_query, :page, :order
+
+  # Bootstrap Table Attributes
+  optional :variants
+
+  def prepare
+    @collection_id = bs_id ||= "smart-table"
+    # model = base_query.model
+    # model_name = model.model_name
+
+    my_base_query = base_query
+    # my_filtered_query = my_base_query
+
+    # Core Collection
+    @collection = set_collection(
+      id: @collection_id,
+      data: my_base_query
+    )
+  end
 
   def response
     # automatically generated table for a given array, hash or collection
     # needs to be evaluated how you would pass in a collection and attributes you want to display
+    table table_attributes do
+      table_head_partial
+      table_body_partial
+      table_footer_partial
+    end
+  end
+
+  protected
+  
+  def table_head_partial
+    thead do
+      tr do
+        th attributes: { 'scope': 'col' }, text: "#"
+        th attributes: { 'scope': 'col' }, text: "First"
+        th attributes: { 'scope': 'col' }, text: "Last"
+        th attributes: { 'scope': 'col' }, text: "Handle"
+      end
+    end
+  end
+
+  def table_body_partial
+    tbody do
+      tr do
+        th attributes: { 'scope': 'row' }, text: "1"
+        td text: "Mark"
+        td text: "Otto"
+        td text: "@mdo"
+      end
+      tr do
+        th attributes: { 'scope': 'row' }, text: "2"
+        td text: "Mark"
+        td text: "Otto"
+        td text: "@mdo"
+      end
+      tr do
+        th attributes: { 'scope': 'row' }, text: "3"
+        td text: "Mark"
+        td text: "Otto"
+        td text: "@mdo"
+      end
+    end
+  end
+
+  def table_footer_partial
+    tfoot do
+      tr do
+        th text: "Footer"
+        td text: "Footer"
+        td text: "Footer"
+        td text: "@Footer"
+      end
+    end
+  end
+
+  def table_attributes
+    html_attributes.merge(
+      id: @collection_id,
+      class: table_classes
+    )
+  end
+
+  def table_classes
+    [].tap do |classes|
+      classes << "table"
+      classes << bs_class
+    end.join(' ').strip
   end
 
 end
--- app/matestack/bootstrap/pages/home.rb
+++ app/matestack/bootstrap/pages/home.rb
@@ -70,23 +70,26 @@ class Bootstrap::Pages::Home < Matestack::Ui::Page
                     {type: :transition, path: "/", text: "Contact"}]  do
                       btn text: "Login"
                     end
-    navigation items: [
-      { type: :link, path: "#home", text: "Home" },
-      { type: :link, path: "#profile", text: "Profile" },
-      { type: :link, path: "#messages", text: "Message" }
-    ]#, pills: true, vertical: true
+    # navigation items: [
+    #   { type: :link, path: "#home", text: "Home" },
+    #   { type: :link, path: "#profile", text: "Profile" },
+    #   { type: :link, path: "#messages", text: "Message" }
+    # ]#, pills: true, vertical: true
 
-    div class: "tab-content" do
-      div class: "tab-pane active", id: "home", attributes: { role: "tabpanel", 'aria-labelledby':  "home-tab" } do
-        plain "Cillum ad ut irure tempor velit nostrud occaecat ullamco aliqua anim Lorem sint. Veniam sint duis incididunt do esse magna mollit excepteur laborum qui. Id id reprehenderit sit est eu aliqua occaecat quis et velit excepteur laborum mollit dolore eiusmod. Ipsum dolor in occaecat commodo et voluptate minim reprehenderit mollit pariatur. Deserunt non laborum enim et cillum eu deserunt excepteur ea incididunt minim occaecat."
-      end
-      div class: "tab-pane", id: "profile", attributes: { role: "tabpanel", 'aria-labelledby': "profile-tab" } do
-        plain "..........ullamco aliqua anim Lorem sint. Veniam sint duis incididunt do esse magna mollit excepteur laborum qui. Id id reprehenderit sit est eu aliqua occaecat quis et velit excepteur laborum mollit dolore eiusmod. Ipsum dolor in occaecat commodo et voluptate minim reprehenderit mollit pariatur. Deserunt non laborum enim et cillum eu deserunt excepteur ea incididunt minim occaecat."
-      end
-      div class: "tab-pane", id: "messages", attributes: {  role: "tabpanel", 'aria-labelledby': "messages-tab" } do
-        plain "do esse magna mollit excepteur laborum qui. Id id reprehenderit sit est eu aliqua occaecat quis et velit excepteur laborum mollit dolore eiusmod. Ipsum dolor in occaecat commodo et voluptate minim reprehenderit mollit pariatur. Deserunt non laborum enim et cillum eu deserunt excepteur ea incididunt minim occaecat."
-      end
-    end
+    # div class: "tab-content" do
+    #   div class: "tab-pane active", id: "home", attributes: { role: "tabpanel", 'aria-labelledby':  "home-tab" } do
+    #     plain "Cillum ad ut irure tempor velit nostrud occaecat ullamco aliqua anim Lorem sint. Veniam sint duis incididunt do esse magna mollit excepteur laborum qui. Id id reprehenderit sit est eu aliqua occaecat quis et velit excepteur laborum mollit dolore eiusmod. Ipsum dolor in occaecat commodo et voluptate minim reprehenderit mollit pariatur. Deserunt non laborum enim et cillum eu deserunt excepteur ea incididunt minim occaecat."
+    #   end
+    #   div class: "tab-pane", id: "profile", attributes: { role: "tabpanel", 'aria-labelledby': "profile-tab" } do
+    #     plain "..........ullamco aliqua anim Lorem sint. Veniam sint duis incididunt do esse magna mollit excepteur laborum qui. Id id reprehenderit sit est eu aliqua occaecat quis et velit excepteur laborum mollit dolore eiusmod. Ipsum dolor in occaecat commodo et voluptate minim reprehenderit mollit pariatur. Deserunt non laborum enim et cillum eu deserunt excepteur ea incididunt minim occaecat."
+    #   end
+    #   div class: "tab-pane", id: "messages", attributes: {  role: "tabpanel", 'aria-labelledby': "messages-tab" } do
+    #     plain "do esse magna mollit excepteur laborum qui. Id id reprehenderit sit est eu aliqua occaecat quis et velit excepteur laborum mollit dolore eiusmod. Ipsum dolor in occaecat commodo et voluptate minim reprehenderit mollit pariatur. Deserunt non laborum enim et cillum eu deserunt excepteur ea incididunt minim occaecat."
+    #   end
+    # end
+    
+    smart_table 
+
     btn text: "Launch Modal", data: { toggle: 'modal', target: '#staticBackdrop' }
     modal id: 'staticBackdrop', header: "Modal Title", body: "Modal Messages", footer: "Close",
     centered: true, scrollable: true, size: :lg    
