83937d01c2522f7ef49a157c7ce7e77411c3f513
Jonas Jabari <jonas@basemate.com>
2020-11-26 17:37:11 +0100

    added dockerized dev and deployment

---
+++ create_env.sh
@@ -0,0 +1,6 @@
+echo "" >> ./.env
+echo "SECRET_KEY_BASE=$SECRET_KEY_BASE" >> ./.env
+echo "MAILER_ADDRESS=$MAILER_ADDRESS" >> ./.env
+echo "MAILER_DOMAIN=$MAILER_DOMAIN" >> ./.env
+echo "MAILER_PASSWORD=$MAILER_PASSWORD" >> ./.env
+echo "MAILER_USER=$MAILER_USER" >> ./.env
