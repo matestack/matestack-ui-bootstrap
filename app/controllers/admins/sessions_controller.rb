7e76bcf34eb059361b7e7a811a906ebdd4d3d4da
Jonas Jabari <jonas@basemate.com>
2020-12-02 13:48:43 +0100

    fixed session controller

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Admins::SessionsController < Devise::SessionsController
   # include your component registry in order to use custom components
+  include Matestack::Ui::Core::ApplicationHelper
   include Bootstrap::Registry
 
   # matestack_app Profile::App # specify the corresponding app to wrap pages in
