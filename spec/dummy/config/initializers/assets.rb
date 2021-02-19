904d015eb79b39c0e75a484bd935ef9ff717de56
Jonas Jabari <jonas@basemate.com>
2021-02-19 13:43:11 +0100

    quick fix missing precompiled assets

--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -6,10 +6,10 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('app/matestack/components')
+# Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# Rails.application.config.assets.paths << Rails.root.join('app/matestack/components')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( application_with_turbolinks.js )
+# Rails.application.config.assets.precompile += %w( application_with_turbolinks.js )
