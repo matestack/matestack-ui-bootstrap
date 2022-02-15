41f0b36be9f95665658d550b33d4cc79eff3616d
Jonas Jabari <jonas@basemate.com>
2022-02-15 10:34:21 +0100

    fixed migrations in order to support rails 5.2 in ci

--- spec/dummy/db/migrate/20201116135026_create_order_items.rb
+++ spec/dummy/db/migrate/20201116135026_create_order_items.rb
@@ -1,4 +1,4 @@
-class CreateOrderItems < ActiveRecord::Migration[6.0]
+class CreateOrderItems < ActiveRecord::Migration[5.2]
   def change
     create_table :order_items do |t|
       t.references :order, null: false, foreign_key: true
--- spec/dummy/db/migrate/20201117083506_devise_create_admins.rb
+++ spec/dummy/db/migrate/20201117083506_devise_create_admins.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class DeviseCreateAdmins < ActiveRecord::Migration[6.0]
+class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
   def change
     create_table :admins do |t|
       ## Database authenticatable
